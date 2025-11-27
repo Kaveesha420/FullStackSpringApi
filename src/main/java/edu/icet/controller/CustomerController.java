2825452beee6af8ac37259f7f3b0bfc067a6695c
Kaveesha420 <icetkaveesha@gmail.com>
2025-11-28 02:06:26 +0530

    Create CustomerController addCustomer method

--- src/main/java/edu/icet/controller/CustomerController.java
+++ src/main/java/edu/icet/controller/CustomerController.java
@@ -1,15 +1,23 @@
 package edu.icet.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import edu.icet.model.Dto.CustomerDto;
+import edu.icet.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("customer")
+@RequiredArgsConstructor
 public class CustomerController {
 
-    @GetMapping("test")
-    public String test(){
-        return "badu wada";
+    final CustomerService customerService;
+  //  CustomerController(CustomerService customerService){
+  //      this.customerService=customerService;
+  //  } // Meka wenuwata field eka final karala lombok use karala @RequiredArgsConstructor anotation eka dammama mekama karaganna puluvan
+
+    @PostMapping("/addCustomer")
+    public String addCustomer(@RequestBody CustomerDto customerDto){
+        return customerService.addCustomer(customerDto);
     }
+
 }
