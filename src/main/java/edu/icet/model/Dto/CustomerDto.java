171ee3107b6ca0d997c4c073b595c55bca5345e2
Kaveesha420 <icetkaveesha@gmail.com>
2025-11-28 01:00:07 +0530

    Create Customer Dto,Entity class

--- src/main/java/edu/icet/model/Dto/CustomerDto.java
+++ src/main/java/edu/icet/model/Dto/CustomerDto.java
@@ -1,4 +1,16 @@
 package edu.icet.model.Dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class CustomerDto {
+    private String id;
+    private String name;
+    private String age;
+    private String address;
+    private String nic;
 }
--- src/main/java/edu/icet/model/Entiti/Customer.java
+++ src/main/java/edu/icet/model/Entiti/Customer.java
@@ -1,4 +1,21 @@
 package edu.icet.model.Entiti;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
 public class Customer {
+        @Id
+        private String id;
+        private String name;
+        private String age;
+        private String address;
+        private String nic;
+
 }
