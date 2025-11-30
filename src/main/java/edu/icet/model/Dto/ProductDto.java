e8641a7f87a0da3fb9994c79361a31a3721a88b2
Kaveesha420 <icetkaveesha@gmail.com>
2025-11-30 16:29:17 +0530

    Set Model layer ProductDto class

--- src/main/java/edu/icet/model/Dto/ProductDto.java
+++ src/main/java/edu/icet/model/Dto/ProductDto.java
@@ -1,4 +1,16 @@
 package edu.icet.model.Dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class ProductDto {
+    private String id;
+    private String name;
+    private String description;
+    private String qty;
+    private String unitPrice;
 }
